6bd66026eaf476dcec3abdb0f58df0754c10ef3c
Wesley Sum <77678835+sumtzehern@users.noreply.github.com>
2023-10-15 02:59:08 -0400

    dark mode and circular img

--- script.js
+++ script.js
@@ -3,4 +3,12 @@ function toggleMenu() {
     const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open");
     icon.classList.toggle("open");
-}
+  }
+
+  const moonIcon = document.getElementById('dark-icon');
+
+// Add a click event listener to the moon icon
+moonIcon.addEventListener('click', function() {
+  // Toggle the 'dark-mode' class on the body element
+  document.body.classList.toggle('dark-mode');
+});
